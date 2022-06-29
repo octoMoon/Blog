20a06a7a4788bff05b94768b05a37fc1a1be81c4
redcat <xvaldmir88x@gmail.com>
2022-06-29 23:07:15 +0700

    view post for id

--- springblog/src/main/java/com/example/springblog/controller/BlogController.java
+++ springblog/src/main/java/com/example/springblog/controller/BlogController.java
@@ -6,10 +6,13 @@ package com.example.springblog.controller;
 
 import com.example.springblog.entity.Post;
 import com.example.springblog.repository.PostRepo;
+import java.util.ArrayList;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -42,5 +45,17 @@ public class BlogController {
         Post post = new Post(title, anons, fullText);
         repo.save(post);
         return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+        if (repo.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> post = repo.findById(id);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blogDetails";
     }
 }
